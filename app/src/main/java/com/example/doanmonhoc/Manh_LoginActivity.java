a290ce7b662e2208675e6f4c9420f8ef0c53db4c
whisky318 <maituanmanh318@gmail.com>
2023-05-17 10:53:12 +0700

    Manh

--- app/src/main/java/com/example/doanmonhoc/Manh_LoginActivity.java
+++ app/src/main/java/com/example/doanmonhoc/Manh_LoginActivity.java
@@ -8,21 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class Manh_LoginActivity extends AppCompatActivity {
 
     TextView txtSignUp;
     Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.manh_activity_login);
         txtSignUp = findViewById(R.id.txtSignUp);
         btnLogin = findViewById(R.id.btnSignIn);
 
         txtSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                Intent intent = new Intent(Manh_LoginActivity.this, Manh_SignUpActivity.class);
                 startActivity(intent);
                 finish();
             }
