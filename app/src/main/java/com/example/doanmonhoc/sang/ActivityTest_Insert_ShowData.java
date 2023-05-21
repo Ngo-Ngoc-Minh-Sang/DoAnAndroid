cdff5437473182ed90c67d62edbf34e57fcc447b
Truong Hoai Thanh Phong <truonghoaithanhphong2023@gmail.com>
2023-05-22 00:18:34 +0700

    Phong commit

--- app/src/main/java/com/example/doanmonhoc/sang/ActivityTest_Insert_ShowData.java
+++ app/src/main/java/com/example/doanmonhoc/sang/ActivityTest_Insert_ShowData.java
@@ -27,12 +27,6 @@ import java.util.List;
 public class ActivityTest_Insert_ShowData extends AppCompatActivity {
 
     Button buttonInsert, buttonShow, buttonDelete, buttonUpdate;
-<<<<<<< HEAD
-    ThuChiDAO thuChiDAO;
-//    NguoiDungDAO ngDungDAO;
-//    HinhAnhGhiChuDAO anhNoteDAO;
-    //DanhMucDAO danhMucDAO;
-=======
 
     ThuChiDAO thuChiDAO;
 
@@ -41,7 +35,6 @@ public class ActivityTest_Insert_ShowData extends AppCompatActivity {
 //    NguoiDungDAO ngDungDAO;
 //    HinhAnhGhiChuDAO anhNoteDAO;
 //    DanhMucDAO danhMucDAO;
->>>>>>> 5a1a8bc906ada3fa4d799a8e97e5f15f8b576252
     Context context;
     List<String> list;
     ListView lv;
@@ -62,16 +55,6 @@ public class ActivityTest_Insert_ShowData extends AppCompatActivity {
         buttonUpdate = (Button) findViewById(R.id.button7);
         lv = (ListView) findViewById(R.id.lvData);
         context = this;
-<<<<<<< HEAD
-       thuChiDAO = new ThuChiDAO(context);
-//        ngDungDAO = new NguoiDungDAO(context);
-//        anhNoteDAO = new HinhAnhGhiChuDAO(context);
-  //      danhMucDAO = new DanhMucDAO(context);
-        buttonInsert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               ThuChiModel thuChi = new ThuChiModel(2, 1, 3000, 3, 4, "2023-05-21", "Lương" );
-=======
         thuChiDAO = new ThuChiDAO(context);
 //        ngDungDAO = new NguoiDungDAO(context);
 //        anhNoteDAO = new HinhAnhGhiChuDAO(context);
@@ -80,22 +63,14 @@ public class ActivityTest_Insert_ShowData extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 ThuChiModel thuChi = new ThuChiModel(1, 2, 2000, 3, 4, "2023-05-19", "Mua sữa chua" );
->>>>>>> 5a1a8bc906ada3fa4d799a8e97e5f15f8b576252
 //                NguoiDungModel ngDung = new NguoiDungModel(1, 2002, 200000, "Ngô Ngọc Minh Sang", "0946885098", "avatar.png", "ngos810@gmail.com", "sang123", "Nữ");
 //                HinhAnhGhiChuModel anhNote = new HinhAnhGhiChuModel(1, 1, "anhNote.png");
 //                DanhMucModel danhMuc = new DanhMucModel(1, "Ăn uống", "icon.png");
 
-<<<<<<< HEAD
-               int kq = thuChiDAO.insertThuChi(thuChi);
-//                int kq = ngDungDAO.insertNguoiDung(ngDung);
-//                int kq = anhNoteDAO.insertHinhAnhNote(anhNote);
-  //              int kq = danhMucDAO.insertDanhMuc(danhMuc);
-=======
                 int kq = thuChiDAO.insertThuChi(thuChi);
 //                int kq = ngDungDAO.insertNguoiDung(ngDung);
 //                int kq = anhNoteDAO.insertHinhAnhNote(anhNote);
 //                int kq = danhMucDAO.insertDanhMuc(danhMuc);
->>>>>>> 5a1a8bc906ada3fa4d799a8e97e5f15f8b576252
                 if(kq == 1)
                     Toast.makeText(ActivityTest_Insert_ShowData.this, "Thành công", Toast.LENGTH_LONG).show();
                 else
@@ -105,17 +80,10 @@ public class ActivityTest_Insert_ShowData extends AppCompatActivity {
         buttonShow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-<<<<<<< HEAD
-               list = thuChiDAO.getALlGiaoDichToString();
-//                list = ngDungDAO.getALlNguoiDungToString();
-//                list = anhNoteDAO.getALlHinhAnhNoteToString();
-    //            list = danhMucDAO.getALlDanhMucToString();
-=======
                 list = thuChiDAO.getALlGiaoDichToString();
 //                list = ngDungDAO.getALlNguoiDungToString();
 //                list = anhNoteDAO.getALlHinhAnhNoteToString();
 //                list = danhMucDAO.getALlDanhMucToString();
->>>>>>> 5a1a8bc906ada3fa4d799a8e97e5f15f8b576252
                 adapter = new ArrayAdapter(context, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, list);
                 lv.setAdapter(adapter);
             }
